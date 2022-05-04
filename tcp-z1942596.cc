d92960d4f04227165d7e7c65c947fcfc10624362
Eric Kellogg <ekellogg3@gmail.com>
2022-05-04 15:43:49 -0500

    got the server running. need to add code to download files

--- tcp-z1942596.cc
+++ tcp-z1942596.cc
@@ -3,38 +3,77 @@
 #include <sys/socket.h>
 #include <dirent.h>
 #include <sys/stat.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <cstring>
 
-int main()
+int main(int argc, char *argv[])
 {
-	while(true)
+	if(argc < 2)
+		std::cout << "not enogh arguments" << std::endl;
+	char buffer[257];
+	int sock, newSock;
+	socklen_t serverlen, clientlen;
+	ssize_t received;
+	struct sockaddr_in echoserver;
+	struct sockaddr_in echoclient;
+
+	if((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0)
 	{
-		connSock = accept(sock, (struct sockaddr *) &echoclient, &clientlen);
-		if(connSock < 0)
-		{
-			perror("accept failed");
-			exit(EXIT_FAILURE);
-		}
+		perror("Failed to create socket");
+		exit(EXIT_FAILURE);
+	}
+	
+	memset(&echoserver, 0, sizeof(echoserver));
+	echoserver.sin_family = AF_INET;
+	echoserver.sin_addr.s_addr = INADDR_ANY;
+	echoserver.sin_port = htons(atoi(argv[1]));
+
+	serverlen = sizeof(echoserver);
+	if(bind(sock, (struct sockaddr *) &echoserver, serverlen) < 0)
+	{
+		perror("Failed to bind server socket");
+		exit(EXIT_FAILURE);
+	}
+	
+	if(listen(sock, 64) < 0)
+	{
+		perror("listen failed");
+		exit(EXIT_FAILURE);
+	}
+	
+	
+	clientlen = sizeof(echoclient);
+	while((newSock = accept(sock, (struct sockaddr *) &echoclient, &clientlen)) != -1)
+	{
 		if(fork())
-			close(connSock);
+		{	
+			close(newSock);
+		}
 		else
 		{
-			if((recieved = read(connSock, buffer, 256)) < 0)
+			//close(0);
+			//dup(newSock);
+			//close(1);
+			//dup(newSock);	
+		
+			std::cerr << "Client connected: " << inet_ntoa(echoclient.sin_addr) << "\n";
+			if((received = read(newSock, buffer, 256)) == -1)
 			{
-				perror("Failed to recieve message");
-				exit(EXIT_FAILURE);
+				perror("read failed");
+				exit(2);
 			}
-			buffer[received] = '\0';
-			cerr << "Client(" << inet_ntoa(echoclient.sin_addr) << ") sent: " << buffer << std::endl;
-			if(write(connSock, buffer, received) != received)
+			if((write(newSock, buffer, received)) != received)
 			{
-				perror("Mismatch in number of echo'd bytes");
-				exit(EXIT_FAILURE);
+				perror("write mismatch");
+				exit(3);
 			}
-			close(connSock);
-			exit(EXIT_SUCCESS);
+			close(newSock);
+			exit(0);
 		}
+		
 	}
-	close(sock);
+	
 
 	return 0;
 }
